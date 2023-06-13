fbe9955258f825808e393a924f10594e71dfa9c8
Sebastien Criqui <jacquesc03@gmail.com>
2023-06-13 15:34:35 -0500

    Created the functionality of the app

---
+++ app/src/main/java/com/example/artspaceapp/Artwork.kt
@@ -0,0 +1,8 @@
+package com.example.artspaceapp
+
+data class Artwork (
+    val imageResourceId: Int,
+    val title: String,
+    val artist: String,
+    val year: Int
+        )
--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,5 +46,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceLayout () {
+    val artworks = arrayOf<Artwork>(
+        //TODO: add in artwork details
+        Artwork(R.drawable.seniorphoto, "Senior Photo", "Sebastien Criqui", 2021),
+        Artwork(R.drawable.background_winterwellredbucky_fall2020__3_, "Bucky", "IDK", 2023)
+   )
 
+    var index by remember {mutableStateOf(0)}
+
+    Column(
+        modifier = Modifier
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(painter = painterResource(id = artworks[index].imageResourceId), contentDescription = artworks[index].toString())
+        Text(text = artworks[index].title)
+        Text(text = "${artworks[index].artist} (${artworks[index].year})")
+        Row() {
+            Button(onClick = { index = (index + artworks.size - 1) % artworks.size}) {
+                Text(text = "Previous")
+           }
+            Button(onClick = { index = (index + 1) % artworks.size}) {
+                Text(text = "Next")
+            }
+        }
+    }
 }
